a6c51618bbd7f69c99d2a6d59c4ecbe6c063614b
Qian Du <du.qian91@gmail.com>
2016-06-29 20:02:59 -0400

    added bootstrap

--- main.js
+++ main.js
@@ -2,13 +2,15 @@ var randomSixDigit = Math.floor(Math.random() * 1000000);
 var forismaticAPIURL = "http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&key=" + randomSixDigit + "&lang=en&jsonp=?";
 
 function randomQuote (){
+
     $.getJSON(forismaticAPIURL, function(data) {
       $("#quote").html(data.quoteText);
       $("#author").html(data.quoteAuthor);
-      $("#tweet").attr("href", ("https://twitter.com/intent/tweet?text=" + data.quoteText + "-- " + data.quoteAuthor));
+      $("#tweet").attr("href", ("https://twitter.com/intent/tweet?text=" + data.quoteText + "- " + data.quoteAuthor));
     });
+
 }
 
 $(document).ready(randomQuote);
-$
+
 $("#generate").on("click", randomQuote);
